143a7682edc580f5f125e81a5abc2a838c6fcf44
jaxQin <53343@163.com>
2024-01-19 16:39:27 +0800

    2

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const Koa = require('koa');
 const app = new Koa();
+const { dbLib } = require('./db.js');
+
 
 app.use(async ctx => {
     console.log(ctx.req.url)
     const urlObj = new URL(ctx.req.url, `http://${ctx.req.headers.host}`);
     const params = urlObj.searchParams;
     console.log('%c 🌮 params: ', 'font-size:20px;background-color: #42b983;color:#fff;', params);
-    ctx.body = 'Hello Vercel, Hi Koa2';
+
+
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
+    const params = urlObj.searchParams;
+    console.log('%c 🌮 params: ', 'font-size:20px;background-color: #42b983;color:#fff;', params);
+
+    let title = params.get('title') || '';
+    let time = params.get('time') || '';
+
+    try {
+        title = decodeURIComponent(params.get('title') || '');
+        time = decodeURIComponent(params.get('time') || '');
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    if (req.url.includes('/add')) {
+        dbLib.add({ time, title });
+        // res.end('This is the /add API');
+    } else if (req.url.includes('/query')) {
+        let r = dbLib.query({ title, time });
+        // res.end(r ? 'true' : 'false');
+        ctx.body = r ? 'true' : 'false';
+
+    } else {
+        // 处理其他请求
+        ctx.body = 'Hello Vercel, Hi Koa2';
+
+    }
 });
 
 
